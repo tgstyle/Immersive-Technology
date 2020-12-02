c579b2f01aa2035114240cc667499b442ef59497
tgstyle <{ID}+{username}@users.noreply.github.com>
2020-12-02 10:35:15 -0700

    More work on the CommonValve TE

--- src/main/java/mctmods/immersivetechnology/common/tileentities/CommonValveTileEntity.java
+++ src/main/java/mctmods/immersivetechnology/common/tileentities/CommonValveTileEntity.java
@@ -1,6 +1,7 @@
 package mctmods.immersivetechnology.common.tileentities;
 
-import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces;
+import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.*;
+import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.IDirectionalTile.PlacementLimitation;
 import blusunrize.immersiveengineering.common.blocks.IEBaseTileEntity;
 import blusunrize.immersiveengineering.common.util.ChatUtils;
 import blusunrize.immersiveengineering.common.util.Utils;
@@ -9,37 +10,53 @@ import mctmods.immersivetechnology.common.util.TranslationKey;
 import mctmods.immersivetechnology.common.util.network.MessageTileSync;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.container.Container;
 import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootContext;
+import net.minecraft.loot.LootParameterSets;
+import net.minecraft.loot.LootParameters;
+import net.minecraft.loot.LootTable;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.fml.common.thread.SidedThreadGroups;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.EnumSet;
 
-public abstract class CommonValveTileEntity extends IEBaseTileEntity implements IEBlockInterfaces.IDirectionalTile, ITickableTileEntity,
-		IEBlockInterfaces.IBlockOverlayText, IEBlockInterfaces.IPlayerInteraction, IEBlockInterfaces.IGuiTile {
+import com.google.common.collect.Lists;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Random;
+
+public abstract class CommonValveTileEntity extends IEBaseTileEntity implements IDirectionalTile, ITickableTileEntity, IBlockOverlayText, IPlayerInteraction, IInteractionObjectIE, IHammerInteraction {
+
+	NonNullList<ItemStack> inventory = NonNullList.withSize(27, ItemStack.EMPTY);
+	public ResourceLocation lootTable;
 
 	final TranslationKey overlayNormal;
 	final TranslationKey overlaySneakingFirstLine;
 	final TranslationKey overlaySneakingSecondLine;
 	final int GuiID;
 
-	public CommonValveTileEntity(TranslationKey overlayNormal, TranslationKey overlaySneakingFirstLine, TranslationKey overlaySneakingSecondLine, int GuiID) {
+	public CommonValveTileEntity(TranslationKey overlayNormal, TranslationKey overlaySneakingFirstLine, TranslationKey overlaySneakingSecondLine) {
 		this.overlayNormal = overlayNormal;
 		this.overlaySneakingFirstLine = overlaySneakingFirstLine;
 		this.overlaySneakingSecondLine = overlaySneakingSecondLine;
-		this.GuiID = GuiID;
 	}
 
 	public Direction facing = Direction.NORTH;
@@ -127,7 +144,7 @@ public abstract class CommonValveTileEntity extends IEBaseTileEntity implements 
 	}
 
 	@Override
-	public String[] getOverlayText(PlayerEntity player, RayTraceResult mop, boolean hammer) {
+	public ITextComponent[] getOverlayText(PlayerEntity player, RayTraceResult mop, boolean hammer) {
 		return player.isSneaking()? new String[] { overlaySneakingFirstLine.format((double)average / 20), overlaySneakingSecondLine.format(packetAverage)} : new String[]{ overlayNormal.format(acceptedAmount) };
 	}
 
@@ -161,32 +178,51 @@ public abstract class CommonValveTileEntity extends IEBaseTileEntity implements 
 	}
 
 	@Override
-	public boolean canOpenGui() {
+	public boolean canUseGui(PlayerEntity player) {
 		return true;
 	}
 
 	@Override
-	public int getGuiID() {
-		return GuiID;
-	}
-
-	@Nullable
-	@Override
-	public TileEntity getGuiMaster() {
+	public IInteractionObjectIE getGuiMaster() {
 		return this;
 	}
 
-	@SideOnly(Side.CLIENT)
-	public abstract void showGui();
+	@Nonnull
+	@Override
+	public Container createMenu(int id, PlayerInventory playerInventory, PlayerEntity player) {
+		if(this.lootTable!=null) {
+			LootTable loottable = this.world.getServer().getLootTableManager().getLootTableFromLocation(this.lootTable);
+			this.lootTable = null;
+			LootContext.Builder contextBuilder = new LootContext.Builder((ServerWorld)this.world);
+			contextBuilder.withParameter(LootParameters.field_237457_g_, Vector3d.copyCentered(pos));
+			if(player!=null) contextBuilder.withLuck(player.getLuck());
+			LootContext context = contextBuilder.build(LootParameterSets.CHEST);
+			Random rand = new Random();
 
-	@SideOnly(Side.CLIENT)
+			List<ItemStack> list = loottable.generate(context);
+			List<Integer> listSlots = Lists.newArrayList();
+			for(int i = 0; i < inventory.size(); i++)
+				if(inventory.get(i).isEmpty())
+					listSlots.add(i);
+			Collections.shuffle(listSlots, rand);
+			if(!listSlots.isEmpty()) {
+				Utils.shuffleLootItems(list, listSlots.size(), rand);
+				for(ItemStack itemstack : list) {
+					int slot = listSlots.remove(listSlots.size()-1);
+					inventory.set(slot, itemstack);
+				}
+				this.markDirty();
+			}
+		}
+		return IInteractionObjectIE.super.createMenu(id, playerInventory, player);
+	}
+
 	@Override
 	public void receiveMessageFromServer(CompoundNBT message) {
 		if(message.contains("packetLimit")) {
 			packetLimit = message.getInt("packetLimit");
 			timeLimit = message.getInt("timeLimit");
 			keepSize = message.getInt("keepSize");
-			showGui();
 		} else {
 			packetAverage = message.getInt("packets");
 			average = message.getLong("average");
@@ -215,8 +251,6 @@ public abstract class CommonValveTileEntity extends IEBaseTileEntity implements 
 		return false;
 	}
 
-
-
 	@Override
 	public Direction getFacing() {
 		return this.facing;
@@ -228,8 +262,8 @@ public abstract class CommonValveTileEntity extends IEBaseTileEntity implements 
 	}
 
 	@Override
-	public int getFacingLimitation() {
-		return 0;
+	public PlacementLimitation getFacingLimitation() {
+		return null;
 	}
 
 	@Override
@@ -238,8 +272,8 @@ public abstract class CommonValveTileEntity extends IEBaseTileEntity implements 
 	}
 
 	@Override
-	public boolean canHammerRotate(Direction side, float hitX, float hitY, float hitZ, LivingEntity entity) {
-		return !entity.isSneaking();
+	public boolean hammerUseSide(Direction side, PlayerEntity player, Hand hand, Vector3d hitVec) {
+		return !player.isSneaking();
 	}
 
 	@Override
